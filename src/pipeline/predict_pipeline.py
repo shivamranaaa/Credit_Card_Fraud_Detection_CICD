1923591e6a55d207d6573800c8800f546cc2efa5
Shivam Rana <shivamrana12321.com>
2023-08-23 17:46:04 +0530

    CICD Deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+import os
 
 
 class PredictPipeline:
@@ -11,13 +12,13 @@ class PredictPipeline:
     def predict(self,features):
         try:
             model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            # preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
+            # preprocessor=load_object(file_path=preprocessor_path)
             print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            # data_scaled=preprocessor.transform(features)
+            preds=model.predict(features)
             return preds
         
         except Exception as e:
@@ -27,38 +28,102 @@ class PredictPipeline:
 
 class CustomData:
     def __init__(  self,
-        gender: str,
-        race_ethnicity: str,
-        parental_level_of_education,
-        lunch: str,
-        test_preparation_course: str,
-        reading_score: int,
-        writing_score: int):
+        Time : float,
+        V1: float,
+        V2 : float,
+        V3 : float,
+        V4 : float,
+        V5 : float,
+        V6 : float,
+        V7 : float,
+        V8 : float,
+        V9 : float,
+        V10 : float,
+        V11 : float,
+        V12 : float,
+        V13 : float,
+        V14 : float,
+        V15 : float,
+        V16 : float,
+        V17 : float,
+        V18 : float,
+        V19 : float,
+        V20 : float,
+        V21 : float,
+        V22 : float,
+        V23 : float,
+        V24 : float,
+        V25 : float,
+        V26 : float,
+        V27 : float,
+        V28 : float,
+        Amount : float
+        ):
 
-        self.gender = gender
-
-        self.race_ethnicity = race_ethnicity
-
-        self.parental_level_of_education = parental_level_of_education
-
-        self.lunch = lunch
-
-        self.test_preparation_course = test_preparation_course
-
-        self.reading_score = reading_score
-
-        self.writing_score = writing_score
+        self.Time = Time
+        self.V1 = V1
+        self.V2 = V2
+        self.V3 = V3
+        self.V4 = V4 
+        self.V5 = V5 
+        self.V6 = V6 
+        self.V7 = V7 
+        self.V8 = V8 
+        self.V9 = V9 
+        self.V10 = V10 
+        self.V11 = V11
+        self.V12 = V12
+        self.V13 = V13 
+        self.V14 = V14 
+        self.V15 = V15 
+        self.V16 = V16 
+        self.V17 = V17 
+        self.V18 = V18 
+        self.V19 = V19 
+        self.V20 = V20 
+        self.V21 = V21 
+        self.V22 = V22 
+        self.V23 = V23 
+        self.V24 = V24 
+        self.V25 = V25 
+        self.V26 = V26 
+        self.V27 = V27 
+        self.V28 = V28 
+        self.Amount = Amount 
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
-                "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
-                "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "Time" : [self.Time],
+                "V1" : [self.V1],
+                "V2" : [self.V2],
+                "V3" : [self.V3],
+                "V4" : [self.V4], 
+                "V5" : [self.V5], 
+                "V6" : [self.V6], 
+                "V7" : [self.V7], 
+                "V8" : [self.V8], 
+                "V9" : [self.V9], 
+                "V10" : [self.V10], 
+                "V11" : [self.V11],
+                "V12" : [self.V12],
+                "V13" : [self.V13], 
+                "V14" : [self.V14], 
+                "V15" : [self.V15], 
+                "V16" : [self.V16], 
+                "V17" : [self.V17], 
+                "V18" : [self.V18], 
+                "V19" : [self.V19], 
+                "V20" : [self.V20], 
+                "V21" : [self.V21], 
+                "V22" : [self.V22], 
+                "V23" : [self.V23], 
+                "V24" : [self.V24], 
+                "V25" : [self.V25], 
+                "V26" : [self.V26], 
+                "V27" : [self.V27], 
+                "V28" : [self.V28], 
+                "Amount" : [self.Amount]
             }
 
             return pd.DataFrame(custom_data_input_dict)
